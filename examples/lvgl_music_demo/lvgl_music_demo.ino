fb183198ad1321a16b73c1d456c83c7c3a1ca0aa
Arvind Singh <vindar@users.noreply.github.com>
2023-06-10 00:38:06 +0200

    bugfix

--- examples/lvgl_music_demo/lvgl_music_demo.ino
+++ examples/lvgl_music_demo/lvgl_music_demo.ino
@@ -139,9 +139,9 @@ void setup()
     while (!tft.begin(SPI_SPEED));      // init
     tft.setFramebuffer(internal_fb);    // set the internal framebuffer
     tft.setDiffBuffers(&diff1, &diff2); // set the diff buffers
-    tft.setRotation(1);                 // landscape mode 1 : 320x240
+    tft.setRotation(0);                 // landscape mode 1 : 320x240
     tft.setDiffGap(4);                  // with have large 8K diff buffers so we can use a small gap. 
-    tft.setVSyncSpacing(1);             // lvgl is already controlling framerate: we just set this to 1 to minimize screen tearing. 
+    tft.setVSyncSpacing(0);             // lvgl is already controlling framerate 
     tft.clear(0);                       // black screen to start. 
 
 
